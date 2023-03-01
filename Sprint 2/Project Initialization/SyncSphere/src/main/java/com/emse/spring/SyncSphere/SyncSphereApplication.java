bf8999a71980a3407d98252659274431e91d8ffd
Nushrat JAHAN <nushrat.jahan@etu.emse.fr>
2023-03-01 09:30:47 +0100

    Adding Initializaed spring boot project

---
+++ Sprint 2/Project Initialization/SyncSphere/src/main/java/com/emse/spring/SyncSphere/SyncSphereApplication.java
@@ -0,0 +1,13 @@
+package com.emse.spring.SyncSphere;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SyncSphereApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SyncSphereApplication.class, args);
+	}
+
+}
---
+++ Sprint 2/Project Initialization/SyncSphere/src/test/java/com/emse/spring/SyncSphere/SyncSphereApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emse.spring.SyncSphere;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SyncSphereApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
