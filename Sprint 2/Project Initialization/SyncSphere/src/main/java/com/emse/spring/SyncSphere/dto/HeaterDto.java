b575e568c2c75bc85fb19c98dad5fedf25e22457
Nushrat JAHAN <nushrat.jahan@etu.emse.fr>
2023-03-01 11:30:48 +0100

    Added HeaterDto

---
+++ Sprint 2/Project Initialization/SyncSphere/src/main/java/com/emse/spring/SyncSphere/dto/HeaterDto.java
@@ -0,0 +1,76 @@
+package com.emse.spring.SyncSphere.dto;
+import com.emse.spring.SyncSphere.model.*;
+
+
+public class HeaterDto {
+    private Long id;
+    private String name;
+    private Long power;
+    private HeaterStatus heaterStatus;
+    private Long roomId;
+    private String roomName;
+
+
+    public HeaterDto() {
+    }
+
+    public HeaterDto(Heater heater) {
+        this.id = heater.getId();
+        this.name = heater.getName();
+        this.power = heater.getPower();
+        this.heaterStatus = heater.getHeaterStatus();
+        this.roomId = heater.getRoom().getId();
+        this.roomName = heater.getRoom().getName();
+    }
+
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public Long getId() {
+        return id;
+    }
+
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getName() {
+        return name;
+    }
+
+
+    public void setPower(Long power) {
+        this.power = power;
+    }
+    public Long getPower() {
+        return power;
+    }
+
+
+    public void setHeaterStatus(HeaterStatus heaterStatus) {
+        this.heaterStatus = heaterStatus;
+    }
+    public HeaterStatus getHeaterStatus() {
+        return heaterStatus;
+    }
+
+
+    public void setRoomId(Long roomId) {
+        this.roomId = roomId;
+    }
+    public Long getRoomId() {
+        return roomId;
+    }
+
+
+    public void setRoomName(String roomName) {
+        this.roomName = roomName;
+    }
+    public String getRoomName() {
+        return roomName;
+    }
+
+
+
+}
